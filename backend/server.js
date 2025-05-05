7dad51082d9991484f8f85cd03c695bdfd91e61e
Galvas <galvaomercante@gmail.com>
2025-05-05 14:12:19 -0300

    ajustes server.js

--- backend/server.js
+++ backend/server.js
@@ -66,7 +66,7 @@ app.get('/convert', async (req, res) => {
 
 
 })
-
-app.listen(3001, () => {
-  console.log('http://localhost:3001/convert')
+const port = process.env.PORT || 3001;
+app.listen(port, () => {
+  console.log(`Servidor rodando na porta ${port}`);
 });
